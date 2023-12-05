c21ef013c00635f28eabdee860c1e002924132fe
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-12-05 23:24:46 +0530

    Tic-Tac-Toe - Created the RandomBotPlayingStrategy Class

---
+++ src/strategies/BotPlayingStrategy/RandomBotPlayingStrategy.java
@@ -0,0 +1,23 @@
+package strategies.BotPlayingStrategy;
+
+import models.Board;
+import models.Cell;
+import models.Move;
+import models.Player;
+import models.constants.CellState;
+
+public class RandomBotPlayingStrategy implements BotPlayingStrategy{
+    @Override
+    public Move makeMove(Player player, Board board) {
+        for(int i = 0; i < board.getSize(); i++){
+            for(int j = 0; j < board.getSize(); j++){
+                if(board.getBoard().get(i).get(j).getCellState().equals(CellState.EMPTY)){
+                    board.getBoard().get(i).get(j).setPlayer(player);
+                    board.getBoard().get(i).get(j).setCellState(CellState.FILLED);
+                    return new Move(new Cell(i, j), player);
+                }
+            }
+        }
+        return null;
+    }
+}
